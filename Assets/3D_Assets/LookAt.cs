19d20a273209b2a800fe428b4c018f4a15f88564
juanrd <juanrd87@gmail.com>
2016-03-10 18:21:11 +0100

    Particulas Nuevas

---
+++ Assets/3D_Assets/LookAt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+    public Transform target;
+    
+    void Update() {
+		Vector3 RectifiedTarget = target.position;
+		RectifiedTarget.y = 0;
+    	// Rotate the camera every frame so it keeps looking at the target 
+		transform.LookAt(RectifiedTarget);
+    }
+}
